332950a06ee4df57d24fb3d32a963ee1a58aca0b
NainikYork <nainikmehta25@gmail.com>
2025-01-05 16:45:56 +0530

    babel, eslint and prettier configured

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'prettier',
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+};
+
---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  semi: true,
+  trailingComma: "all",
+  singleQuote: true,
+  jsxSingleQuote: false,
+  tabWidth: 2,
+  printWidth: 80,
+  bracketSpacing: true,
+  endOfLine: "crlf",
+  useTabs: false,
+  arrowParens: "always",
+};
